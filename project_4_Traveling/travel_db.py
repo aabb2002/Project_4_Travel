9bedc34ae5f5b93fef7fc71ee51c131b2c26ffeb
Dan Smestad <hockeydan25@gmail.com>
2022-03-16 21:48:36 -0500

    deleted user.py file and added contents creating event object to travel.db. modified travel_db to validate event add.

--- project_4_Traveling/travel_db.py
+++ project_4_Traveling/travel_db.py
@@ -1,34 +1,33 @@
 import sqlite3
 from datetime import date
-import os
+
 
 db = 'My_Travel_Events.sqlite'  # create datbase and variable is assigned.
 
-
 class Event:
-    def __int__(self, event_name, country, city, currency):
+    def __int__(self, event_name, country, city, currency, current_temp):
         self.event_name = event_name
         self.date = date
         self.country = country
         self.city = city
         self.currency = currency
+        self.current_temp = current_temp
 
-        self.myTravelEvent = MyTravelEvent()
+        self.myTravelEvent = MyTravelEvents()
 
     def save_event(self):
         if self.event_name:
             self.myTravelEvent._add_event(self)
 
 
-class MyTravelEvent:
+class MyTravelEvents:
 
     """ class to hold and manage a list of event. All database objects created are the same object.
     Provides operations to create database add, display/query the database.need to have: IF NOT EXISTS for tables and DB's. """
 
 
-
     def __init__(self):
-        create_table_sql = 'CREATE TABLE IF NOT EXISTS Events (event_name TEXT UNIQUE NOT NULL, event_date DATE, country text, city text, currency text)'
+        create_table_sql = 'CREATE TABLE IF NOT EXISTS MyTravelEvent (event_name TEXT UNIQUE NOT NULL, event_date DATE, country text, city text, currency text, current_temp small int)'
                                  
         con = sqlite3.connect(db)
         
@@ -40,7 +39,7 @@ class MyTravelEvent:
     def get_all_events(self):
         """ returns an events list """
 
-        get_all_events_sql = 'SELECT rowid, * FROM Events'
+        get_all_events_sql = 'SELECT * FROM MyTravelEvent'
 
         con = sqlite3.connect(db)
         con.row_factory = sqlite3.Row
@@ -48,27 +47,33 @@ class MyTravelEvent:
         events = []
 
         for r in rows:
-            event = Event(r['event_name'], r['Date'], r['country'], r['city'], r['temp'], r['curency'])
+            event = Event(r['event_name'], r['Date'], r['country'], r['city'], r['currency'], r['current_temp'])
             event.append(event)
 
-        con.close()            # reading list example book object event object.
-        
+        con.close()            #  example book object event object.
         return events  
 
  
-    def _add_event(self)  #  save button """ Adds evnet to database. Raises RecordError if a event wit (not case sensitive) is already in the database."""   
-        insert_sql = 'INSERT INTO Events (event_name, date, country, city, currency, temp) VALUES (?, ?, ?, ?, ?, ?)'
-        try: 
-            with sqlite3.connect(db) as con:
-                res = con.execute(insert_sql, (event_name.n) )
-                new_event_name = res.lastrowid  # Get the ID of the new row in the table 
-                event_name = new_event_name  # Set this book's ID
-        except sqlite3.IntegrityError as e:
-                raise RecordError(f'Error - this event is in the database. {event_name}') from e
-        finally:
-            con.close()
+    def _add_event(self, event)  #  save button """ Adds evnet to database. Raises RecordError if a event wit (not case sensitive) is already in the database."""   
+        
+        
+        with sqlite3.connect(db) as con:
+            try:             
+                row_add = con.execute('SELECT * FROM MyTravelEvent WHERE event_name like ?', (event,))
+                first_row = row_add.fetchone()
+                if first_row:
+                    raise RecordError(f'Error - this event is in the database. {event}')                 
+                else:
+                    insert_sql = 'INSERT INTO Events (event_name, date, country, city, currency, current_temp) VALUES (?, ?, ?, ?, ?, ?)'
+        
+            except ValueError as e:
+                print('invalid')
+
+                con.close()
 
 
 class RecordError(Exception):
     pass
 
+
+                            
