a71f41081b757c925d17b01d5c3611fc23bfd359
Dan Smestad <hockeydan25@gmail.com>
2022-03-21 22:05:39 -0500

    modified travel_db coments. gitignore added test_My_Travel_Events.sqlite. added testing to Test/test_Travel_db for add_event, delete_all_events, get_all_events. still getting mixed results. I have more work to get them corrected

--- project_4_Traveling/Tests/test_Travel_db.py
+++ project_4_Traveling/Tests/test_Travel_db.py
@@ -1,3 +1,4 @@
+
 import sqlite3
 import unittest
 from unittest import TestCase
@@ -34,31 +35,37 @@ class TestTravelDB(TestCase):
         self.MTE.delete_events_all()
 
 
-    # def test_add_event(self):       
-    #     self.ev = Event("Fun Day", "2022-04-15 12:00:00" , "USA", "Minneapolis", "EUR", 78)
-    #     self.ev.save_event()
-    #     self.assertTrue(self.MTE.exact_match(self.ev))
-    #     self.assertEqual(1, self.ev)
+    def test_add_event(self):       
+        self.ev = Event("Fun Day", "2022-04-15 12:00:00" , "USA", "Minneapolis", "EUR", 78)
+        self.ev.save_event()
+        index = self.MTE.get_all_events()
+        self.assertEquals(self.ev, index)
 
 
-    def test_add_event_to__database_with_data(self):
-        self.add_test_data()
-        self.ev0 = Event("Concert", "2022-04-15 12:00:00", "USA", "Minneapolis", "EUR", 78)
-        self.ev0.save()
-        self.assertTrue(self.Events.exact_match(self.ev0))
-        self.assertEqual(1, self.ev0)            
+    # def test_add_event_to_database_with_data(self):
+    #     self.add_test_data()
+    #     self.ev0 = Event("Concert", "2022-04-15 12:00:00", "USA", "Minneapolis", "EUR", 78)
+    #     self.ev0.save_event()
+    #     #self.assertTrue(self.Events.exact_match(self.ev0))
+    #     self.assertEqual(4, self.ev0)            
 
 
     # def test_get_all_events(self):
     #     self.add_test_data()
-    #     self.assertCountEqual([self.ev1, self.ev2, self.ev3], self.test_get_all_events())
+    #     self.assertCountEqual([self.ev1, self.ev2, self.ev3], self.MTE.get_all_events())
 
 
     # def test_is_data_in_database(self):
     #     self.add_test_data()
-    #     self.assertTrue(self.Events.exact_match(self.ev1))
-    #     self.assertTrue(self.Events.exact_match(self.ev1))
-    #     self.assertTrue(self.Events.exact_match(self.ev1))
+    #     self.assertTrue(self.MTE.get_all_events())
+
+
+    # def test_delete_all_events(self):
+    #     self.MTE.delete_events_all()
+    #     self.assertEqual(0, self.MTE.get_all_events())        
+
+
+       
 
    
     
