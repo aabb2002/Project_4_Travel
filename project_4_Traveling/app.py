81cbbf0e67e8a4c8a6f0eea8c6fea206cfd83ea2
Dash <123qwe@gmail.com>
2022-03-27 20:45:31 -0500

    Saving to the db

--- project_4_Traveling/app.py
+++ project_4_Traveling/app.py
@@ -63,26 +63,25 @@ def myTravelEventInfo():
 
 @app.route('/saveuserinfo',methods=['POST'])
 def saveuserinfo():
-    for i in range (1,3):
+    for n in range(1,3):
         country = request.form.get('country')
         city = request.form.get('city')
-
         event_name = request.form.get('event')
-    #destin_from_date = request.form.get('destin_from_date')
-    #destin_to_date = request.form.get('destin_to_date')
+        #destin_from_date = request.form.get('destin_from_date')
+        #destin_to_date = request.form.get('destin_to_date')
         print(city, country,event_name)
         save_event_flask = Event(event_name,country,city)
         save_event_flask.save_event()
-    return ('Success')
+        return ('Success')
    # return render_template('saved_destinations.html', saved=saved)
 
 @app.route('/saveduserinfo',methods=['GET'])
 #@app.route('/show_saved')
 def show_saved_destinations():
     # get destinations from the database 
-    #saved = MyTravelEvents()
+    saved = MyTravelEvents()
     #show_events = []
-    show_events = MyTravelEvents.get_all_events()
+    show_events = saved.get_all_events()
     print(show_events)
     #for i in show_events:
     #TODO database object to show on the screen
